3bc23a9325f00248ddb33fb10478b5e89bc787b7
Leonardo <leonardobalbino109@gmail.com>
2026-03-09 00:37:03 -0300

    Add challenge #4.

--- Fundamentos Parte 2/Challenge 4/script.js
+++ Fundamentos Parte 2/Challenge 4/script.js
@@ -22,7 +22,7 @@ const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-for (i = 0; i < bills.length; i++) {
+for (let i = 0; i < bills.length; i++) {
     tips.push(calcTip(bills[i]));
     totals.push(bills[i] + tips[i]);
     console.log(`The bill of the customer number ${i + 1} is ${bills[i]} with a tip of ${tips[i]} and a total of ${totals[i]}`)
@@ -49,3 +49,34 @@ let calcAverage = function (arr) {
 }
 
 console.log(calcAverage(totals));
+
+/* Correção
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum += arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+*/
